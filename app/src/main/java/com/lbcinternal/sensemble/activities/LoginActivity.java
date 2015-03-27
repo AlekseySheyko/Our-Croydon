5053f71bb73ae7b689ddbf3f6d8901702daec3e8
AlekseySheyko <aleksey_sheyko@hotmail.com>
2015-03-27 18:52:28 +0200

    Login layout completed

--- app/src/main/java/com/lbcinternal/sensemble/activities/LoginActivity.java
+++ app/src/main/java/com/lbcinternal/sensemble/activities/LoginActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 import com.lbcinternal.sensemble.R;
 
@@ -13,6 +14,7 @@ public class LoginActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
         setContentView(R.layout.activity_login);
     }
 
