8b310bcd75d2ebc6d45256c66c6237cf0830bb9a
AlekseySheyko <aleksey_sheyko@hotmail.com>
2015-03-27 02:58:24 +0200

    Close writing activity when sent

--- app/src/main/java/com/lbcinternal/sensemble/AddCommentActivity.java
+++ app/src/main/java/com/lbcinternal/sensemble/AddCommentActivity.java
@@ -1,7 +1,12 @@
 package com.lbcinternal.sensemble;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class AddCommentActivity extends ActionBarActivity {
@@ -10,5 +15,17 @@ public class AddCommentActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_comment);
+
+        EditText editText = (EditText) findViewById(R.id.field);
+        editText.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    startActivity(new Intent(AddCommentActivity.this,
+                            CommentsActivity.class));
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
