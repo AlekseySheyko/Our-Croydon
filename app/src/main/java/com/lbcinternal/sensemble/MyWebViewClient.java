728d07509747e479dc5054b54ac91f068dd44030
AlekseySheyko <aleksey_sheyko@hotmail.com>
2015-03-28 12:10:01 +0200

    Progress bar on webview loading

--- app/src/main/java/com/lbcinternal/sensemble/MyWebViewClient.java
+++ app/src/main/java/com/lbcinternal/sensemble/MyWebViewClient.java
@@ -1,12 +1,29 @@
 package com.lbcinternal.sensemble;
 
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import com.lbcinternal.sensemble.activities.CheckEmailActivity;
 
 public class MyWebViewClient extends WebViewClient {
     @Override
     public boolean shouldOverrideUrlLoading(WebView webView, String url) {
-        webView.loadUrl(url);
+        if (url.contains("Account/login.aspx?userRegistered=yes")) {
+            webView.getContext().startActivity(new Intent(
+                    webView.getContext(), CheckEmailActivity.class
+            ));
+        } else {
+            webView.loadUrl(url);
+        }
+        Log.i("TAG", "Url: " + url);
         return true;
+    }
+
+    @Override public void onPageFinished(WebView webView, String url) {
+        super.onPageFinished(webView, url);
+        webView.setVisibility(View.VISIBLE);
     }
 }
---
+++ app/src/main/java/com/lbcinternal/sensemble/activities/CheckEmailActivity.java
@@ -0,0 +1,15 @@
+package com.lbcinternal.sensemble.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.lbcinternal.sensemble.R;
+
+public class CheckEmailActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_email);
+    }
+}
