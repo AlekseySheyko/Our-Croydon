cb51526cfc92cb2af2c7dc67f62802c4e76dd6b4
AlekseySheyko <aleksey_sheyko@hotmail.com>
2015-03-28 14:43:42 +0200

    Make toast when failed to login

--- app/src/main/java/com/lbcinternal/sensemble/activities/LoginActivity.java
+++ app/src/main/java/com/lbcinternal/sensemble/activities/LoginActivity.java
@@ -7,11 +7,15 @@ import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.lbcinternal.sensemble.R;
 import com.lbcinternal.sensemble.rest.ApiService;
 import com.lbcinternal.sensemble.rest.CroydonClient;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 import retrofit.ResponseCallback;
 import retrofit.RetrofitError;
@@ -32,18 +36,28 @@ public class LoginActivity extends Activity {
         String password = ((EditText) findViewById(R.id.passwordField)).getText().toString();
         boolean remember = ((ToggleButton) findViewById(R.id.toggle)).isChecked();
 
-        ApiService service = new CroydonClient(username, password).getApiService();
+        ApiService service = new CroydonClient().getApiService();
         service.login(username, password, remember, new ResponseCallback() {
             @Override public void success(Response response) {
+                try {
+                    InputStream is = response.getBody().in();
+                    String responseString = convertStreamToString(is);
+                    if (responseString.contains("Please log in using your @croydon.gov.uk email address.")) {
+                        Toast.makeText(LoginActivity.this, "Login failed. Check your credentials.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    }
 
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
 
             @Override public void failure(RetrofitError error) {
+                Toast.makeText(LoginActivity.this, "Login failed. Check your credentials.", Toast.LENGTH_SHORT).show();
                 error.printStackTrace();
             }
         });
-
-        startActivity(new Intent(this, MainActivity.class));
     }
 
     public void createAccount(View view) {
@@ -56,5 +70,10 @@ public class LoginActivity extends Activity {
         startActivity(new Intent(this, WebViewActivity.class));
         PreferenceManager.getDefaultSharedPreferences(this)
                 .edit().putString("action", "recover").apply();
+    }
+
+    static String convertStreamToString(java.io.InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
     }
 }
--- app/src/main/java/com/lbcinternal/sensemble/rest/CroydonClient.java
+++ app/src/main/java/com/lbcinternal/sensemble/rest/CroydonClient.java
@@ -1,10 +1,7 @@
 package com.lbcinternal.sensemble.rest;
 
-import android.util.Base64;
-
 import com.squareup.okhttp.OkHttpClient;
 
-import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
 import retrofit.RestAdapter.LogLevel;
 import retrofit.client.OkClient;
@@ -17,35 +14,13 @@ public class CroydonClient {
     private ApiService mApiService;
 
     public CroydonClient() {
-        createService(null, null);
-    }
-
-    public CroydonClient(String username, String password) {
-        createService(username, password);
-    }
-
-    private void createService(String username, String password) {
-        RestAdapter.Builder builder = new RestAdapter.Builder()
+        RestAdapter restAdapter = new RestAdapter.Builder()
                 .setLogLevel(LogLevel.FULL)
                 .setClient(new OkClient(new OkHttpClient()))
-                .setEndpoint(BASE_URL);
+                .setEndpoint(BASE_URL)
+                .build();
 
-        if (username != null && password != null) {
-            final String credentials = username + ":" + password;
-
-            builder.setRequestInterceptor(new RequestInterceptor() {
-                @Override
-                public void intercept(RequestFacade request) {
-                    String string = "Basic " + Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
-                    request.addHeader("Authorization", string);
-                    request.addHeader("Accept", "application/json");
-                }
-            });
-        }
-
-        RestAdapter adapter = builder.build();
-
-        mApiService = adapter.create(ApiService.class);
+        mApiService = restAdapter.create(ApiService.class);
     }
 
     public ApiService getApiService() {
